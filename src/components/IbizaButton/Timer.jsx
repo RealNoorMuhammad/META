3865982138596a1c6b9b122ff5fff59a0d0145a4
Noor Muhammad <95448023+RealNoorMuhammad@users.noreply.github.com>
2023-07-27 15:03:52 -0700

    Done

--- src/components/IbizaButton/Timer.jsx
+++ src/components/IbizaButton/Timer.jsx
@@ -4,7 +4,7 @@ const Timer = () => {
   const calculateTimeRemaining = () => {
     const now = new Date();
     const targetTime = new Date(now);
-    targetTime.setHours(now.getHours() + 4, 0, 0, 0); // Change 6 to 4 for a 4 hours timer
+    targetTime.setHours(now.getHours() + 4, 0, 0, 0); // Set target time to 4 hours from now
 
     const timeDiff = targetTime.getTime() - now.getTime();
     const secondsRemaining = Math.max(0, Math.floor(timeDiff / 1000));
@@ -15,7 +15,13 @@ const Timer = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTimeRemaining(calculateTimeRemaining());
+      setTimeRemaining(prevTime => {
+        if (prevTime === 0) {
+          clearInterval(interval); // Stop the interval when time reaches 00:00:00
+          return 0;
+        }
+        return prevTime - 1;
+      });
     }, 1000);
 
     return () => clearInterval(interval);
@@ -28,7 +34,7 @@ const Timer = () => {
     return `${hours.toString().padStart(2, '0')} : ${minutes.toString().padStart(2, '0')} : ${seconds.toString().padStart(2, '0')}`;
   };
 
-  return <div className='toks_head'>{formatTime(timeRemaining)}</div>;
+  return <div className='toks_head'>{timeRemaining === 0 ? "THE END" : formatTime(timeRemaining)}</div>;
 };
 
 export default Timer;
